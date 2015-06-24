a4b7b6a5c127ee556040cbf55459060f76437d1e
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-06-24 11:11:34 -0400

    Update version bump related scripts for Lithium
    Change-Id: I23e294f0558da610308202bfc49a8f07098b3f85
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ scripts/patch-odl-release.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2015 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Thanh Ha - Initial implementation
+##############################################################################
+
+# The purpose of this patch is to:
+#
+#   1) Apply autorelease patches for a ODL Release
+#   2) Create version bump commit for post-release dev cycle
+
+USAGE="USAGE: patch-odl-release <path-to-patches> <tag>\n\
+\n\
+path-to-patches - The path to the directory containing ODL Release patches\n\
+tag  - example: Lithium-SR1"
+
+if [ -z "$2" ]
+then
+    echo -e "$USAGE"
+    exit 1
+fi
+
+PATCH_DIR=$1
+RELEASE_TAG=$2
+
+project=${PWD##*/}
+scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+git apply ${PATCH_DIR}/${project}.patch
+git commit -asm "Release Lithium"
+find . -name pom.xml | xargs grep SNAPSHOT
+$scriptdir/version.sh bump $RELEASE_TAG
+git commit -asm "Bumping versions by 0.0.1 for next dev cycle"
+find . -name pom.xml | xargs grep $RELEASE_TAG
