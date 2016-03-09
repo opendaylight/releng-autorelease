689ccaaa8b8d2309f61b70825188da45943213e3
Ed Warnicke <eaw@cisco.com>
2016-03-09 10:27:58 -0700

    Initial script to handle version management
    This script is best run from the autorelease root directory after you
    run:
    
        git submodule init
        git submodule update
    
    Use the command line:
    
        find . -name pom.xml | grep -v target | grep -v archetype | time xargs scripts/version_management/extract_version.py
    
    This script outputs all versions of all poms including resolving
    properties up through parents in the form:
    
        groupId:artifactId:version
    
    Change-Id: I9473f5166f228fde26d33358c67ad5052e6de212
    Also-by: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: Ed Warnicke <eaw@cisco.com>
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ scripts/version_management/extract_version.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+from pom import POM
+
+pomdict = dict()
+unresolvepoms = sys.argv[1:]
+parsedpoms = dict()
+lastlen = len(unresolvepoms) + 1
+
+while len(unresolvepoms) > 0:
+    if not (lastlen > len(unresolvepoms)):
+        break
+    lastlen = len(unresolvepoms)
+    for filename in unresolvepoms:
+        pom = parsedpoms.get(filename)
+        if pom is None:
+            pom = POM(filename, pomdict)
+            parsedpoms[filename] = pom
+        else:
+            pom.calculateEffectiveMavenCoordinates()
+        if not ("${" in str(pom.effectiveMavenCoordinates.version)):
+            pomdict[str(pom)] = pom
+            unresolvepoms.remove(filename)
+
+if len(unresolvepoms) > 0:
+    for item in unresolvepoms:
+        pom = parsedpoms[item]
+        version = pom.effectiveMavenCoordinates.version
+        parent = pom.parentMavenCoordinates
+        print("Could not resolve pom: %s looking for %s in parent %s" % (item, version, parent))
+
+    raise Exception("Failure to resolve properties in all pom files")
+
+for key in pomdict:
+    print(key)
