ab4ea40169dbe6aa3e600e814f18fea125dbff9f
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-06-01 13:32:54 -0400

    Create a project versions csv file
    Change-Id: I5c276b11a9cf4d2a966c6185af3bc47130a61678
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ scripts/find-artifact-versions.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation.  All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script finds all the artifact versions in a release and produces a csv
+
+set -eu -o pipefail
+
+print_gav() {
+    file=$1
+
+    artifact_id=$(xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 \
+      -t -m '/x:project' \
+      -v "/x:project/x:artifactId" \
+      $file)
+
+    group_id=$(xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 \
+        -t -m '/x:project' \
+        --if "/x:project/x:groupId" \
+        -v "/x:project/x:groupId" \
+        --elif "/x:project/x:parent/x:groupId" \
+        -v "/x:project/x:parent/x:groupId" \
+        --else -o "" \
+        $file)
+
+    version=$(xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 \
+        -t -m '/x:project' \
+        --if "/x:project/x:version" \
+        -v "/x:project/x:version" \
+        --elif "/x:project/x:parent/x:version" \
+        -v "/x:project/x:parent/x:version" \
+        --else -o "" \
+        $file)
+
+    echo "$group_id,$artifact_id,$version" >> project-versions.csv
+}
+
+export -f print_gav
+
+find . -name pom.xml -exec bash -c 'print_gav "$0"' {} \;
