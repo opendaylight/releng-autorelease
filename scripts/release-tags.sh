c6384d7d543d748b4ba2e9584716b38ef72fff27
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-07-14 11:37:35 -0400

    Move set command to after parameters check
    Otherwise it will fail before it can print the usage statement.
    
    Change-Id: I8517b2a71fe7ebccc218e41fccb529a52c55bb4f
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- scripts/release-tags.sh
+++ scripts/release-tags.sh
@@ -11,14 +11,14 @@ RELEASE_TAG=$1
 PATCH_DIR=$2
 GIT_BUNDLE_URL=$3
 
-# Ensure we fail the if any steps fail.
-set -eu -o pipefail
-
 if [[ -z $3 ]]; then
     echo 'Missing arguments.'
     echo 'Usage: release-tags RELEASE_TAG PATCH_DIR GIT_BUNDLE_URL'
     exit 1
 fi
+
+# Ensure we fail the if any steps fail.
+set -eu -o pipefail
 
 mkdir -p $PATCH_DIR
 pushd $PATCH_DIR
