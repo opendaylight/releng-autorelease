f2ca5a771f208ad69291ed1dfafaa99457c46660
Vratko Polak <vrpolak@cisco.com>
2017-10-12 15:32:53 +0200

    Treat projects equally in determine-merge-order.py
    Remove uncommented conditions which skip some links or nodes.
    
    Also update list-project-dependencies.sh:
    + Coretutorials is not in autorelease, so do not filter out "toaster".
    + splited => splitted
    + sort | uniq => sort -u
    - Temporary debug echos added.
    
    Change-Id: I9c3f971bf60a710343720136c235281f499a69b2
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- scripts/list-project-dependencies.sh
+++ scripts/list-project-dependencies.sh
@@ -31,44 +31,55 @@ fi
 rm -f $LOG_FILE
 
 for module in $modules; do
+    echo "entering module: $module"
     module_dependencies=""
     # shellcheck disable=SC2044
     for pom in $(find "$module" -name pom.xml ! -path "*/src/*" ! -path "*/target/*"); do
-        # Find ODL projects that are dependencies of a module and list them
-        # in a sorted list. Also grep -v to remove invalid projects such as
-        # the "toaster" project from coretutorials.
+        echo "examining pom: $pom"
+        # Find ODL projects that are dependencies of a module and list them in a sorted list.
         dependencies=$(xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 \
                                       -t \
                                       -m '//x:project/x:dependencies/x:dependency' \
                                       -n -v "x:groupId" "$pom" | \
                        grep org.opendaylight | \
-                       sed -e 's@org.opendaylight.@@' | \
-                       grep -v toaster | sort | uniq)
+                       sed -e 's@org.opendaylight.@@' | sort | uniq)
+        echo "got dependencies: $dependencies"
         # Include parent poms as dependencies
         dependencies_parentpoms=$(xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 \
                                       -t \
                                       -m '//x:project/x:parent' \
                                       -n -v "x:groupId" "$pom" | \
                                   grep org.opendaylight | \
-                                  sed -e 's@org.opendaylight.@@' | \
-                                  grep -v toaster | sort | uniq)
-        module_dependencies=$(echo "$module_dependencies" "$dependencies_parentpoms" "$dependencies" | tr " " "\n" | sort | uniq)
+                                  sed -e 's@org.opendaylight.@@' | sort -u)
+        echo "got parentpoms: $dependencies_parentpoms"
+        module_dependencies=$(echo "$module_dependencies" "$dependencies_parentpoms" "$dependencies" | tr " " "\n" | sort -u)
+        echo "current list: $module_dependencies"
     done
 
-    module_dependencies=$(echo "$module_dependencies" | sed 's#\.#/#g')
+    # A set with mdsal, mdsal.model and mdsal.model.binding2 can break the following code.
+    # Reversing the order should work around that.
+    module_dependencies=$(echo "$module_dependencies" | sort -r | sed 's#\.#/#g')
+    echo "slashed list: $module_dependencies"
     for search_module in $module_dependencies; do
+        echo "processing module $search_module"
         if [[ $search_module =~ .*/.* ]]; then
+            echo "slashed module detected"
             if [[ ! $modules =~ .*$search_module.* ]]; then
-                splited_module=$(echo "$search_module" | sed 's/\/.*//')
-                if [[ $modules =~ .*$splited_module.* ]]; then
-                    module_dependencies=$(echo "$module_dependencies" | tr ' ' '\n' | sed -e "s@$search_module@$splited_module@" | sort | uniq)
+                echo "splittable module detected"
+                splitted_module=$(echo "$search_module" | sed 's/\/.*//')
+                echo "splitted: $splitted_module"
+                if [[ $modules =~ .*$splitted_module.* ]]; then
+                    module_dependencies=$(echo "$module_dependencies" | tr ' ' '\n' | sed -e "s@$search_module@$splitted_module@" | sort -u)
+                    echo "list after replacing with one splitted: $module_dependencies"
                 else
-                    module_dependencies=$(echo "$module_dependencies" | tr ' ' '\n' | sed -e "s@$search_module.*@@" | sort | uniq)
+                    module_dependencies=$(echo "$module_dependencies" | tr ' ' '\n' | sed -e "s@$search_module.*@@" | sort -u)
+                    echo "list after removing module: $module_dependencies"
                 fi
             fi
         fi
     done
-    module_dependencies=$(echo "$module_dependencies" | tr ' ' '\n' | sed -e "s@$module@@" | sort | uniq)
+    module_dependencies=$(echo "$module_dependencies" | tr ' ' '\n' | sed -e "s@$module@@" | sort -u)
+    echo "after newlining: $module_dependencies"
 
     if [ "$1" == "dot" ]; then
         for dependency in $(echo "$module_dependencies" | tr "\n" " "); do
