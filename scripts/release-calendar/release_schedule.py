f9bcfaf1aad9fdd7de10a5bf561c37e9e76dec1c
Sam Hague <shague@redhat.com>
2018-07-09 14:24:01 -0400

    add flake8 to tox
    Change-Id: If67a5c8026f2244eaaab4c29a70003cd850f833d
    Signed-off-by: Sam Hague <shague@redhat.com>

--- scripts/release-calendar/release_schedule.py
+++ scripts/release-calendar/release_schedule.py
@@ -6,10 +6,12 @@ import time
 import json
 import argparse
 
+
 def parse_date(date_str):
     """Parse the date string using the date format %m/%d/%Y"""
     date_time = time.strptime(date_str, "%m/%d/%Y")
     return datetime.date(date_time.tm_year, date_time.tm_mon, date_time.tm_mday)
+
 
 def add_weeks_respecting_holidays(date, weeks, holidays):
     """Adds a specified number of weeks to the specified date and adds an additional week for each
@@ -17,16 +19,16 @@ def add_weeks_respecting_holidays(date, weeks, holidays):
     new_date = date + datetime.timedelta(weeks=weeks)
 
     for holiday in holidays:
-        if date < holiday and holiday <= new_date:
-            print("*", end='') # this is a horrible hack to convey when holidays skip
+        if date < holiday <= new_date:
+            print("*", end='')  # this is a horrible hack to convey when holidays skip
             new_date += datetime.timedelta(weeks=1)
 
     return new_date
 
+
 def print_schedule():
     """Parse a specified JSON config file and print an OpenDaylight release schedule based on it."""
-    parser = argparse.ArgumentParser(description=
-                                     "Produce OpenDaylight release schedules based on json config.")
+    parser = argparse.ArgumentParser(description="Produce OpenDaylight release schedules based on json config.")
     parser.add_argument("json", nargs="?", default="schedule.json", type=argparse.FileType('r'),
                         help="path to a JSON config file")
 
@@ -44,15 +46,15 @@ def print_schedule():
 
     curr_milestone_start = start_date
 
-    print("OpenDaylight "+ config["name"] + " Release")
-    print("  Starting on "+config["start"])
-    print("  "+str(len(config["milestones"]))+" milestones with "+
-          str(config["weeks_between_milestones"])+" weeks between them")
-    print("  "+str(len(config["offsets"]))+" "+str(config["offset_in_weeks"])+"-week offsets")
-    print("  "+str(len(config["RCs"]))+" RCs with "+str(config["weeks_between_RCs"])+
-          " weeks between them, starting "+str(config["weeks_before_first_RC"])+
+    print("OpenDaylight " + config["name"] + " Release")
+    print("  Starting on " + config["start"])
+    print("  " + str(len(config["milestones"])) + " milestones with " +
+          str(config["weeks_between_milestones"]) + " weeks between them")
+    print("  " + str(len(config["offsets"])) + " " + str(config["offset_in_weeks"]) + "-week offsets")
+    print("  " + str(len(config["RCs"])) + " RCs with " + str(config["weeks_between_RCs"]) +
+          " weeks between them, starting " + str(config["weeks_before_first_RC"]) +
           " weeks after the last offset of the last milestone")
-    print("  Holidays: "+json.dumps(config["holidays"]))
+    print("  Holidays: " + json.dumps(config["holidays"]))
     print()
 
     for milestone in config["milestones"]:
